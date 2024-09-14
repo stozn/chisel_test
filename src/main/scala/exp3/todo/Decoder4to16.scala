df3d8920f52119331af9a9dfd1036a54a1045f38
stozn <stozn.cn@outlook.com>
2024-09-14 22:01:40 +0800

    update decoder

--- src/main/scala/exp3/todo/Decoder4to16.scala
+++ src/main/scala/exp3/todo/Decoder4to16.scala
@@ -9,26 +9,29 @@ class Decoder4to16 extends Module {
         val out = Output(UInt(16.W))
     })
 
-    io.out := MuxLookup(io.in, 0.U)(
-        Seq(
-            0.U -> "h1".U,
-            1.U -> "h2".U,
-            2.U -> "h4".U,
-            3.U -> "h8".U,
-            4.U -> "h10".U,
-            5.U -> "h20".U,
-            6.U -> "h40".U,
-            7.U -> "h80".U,
-            8.U -> "h100".U,
-            9.U -> "h200".U,
-            10.U -> "h400".U,
-            11.U -> "h800".U,
-            12.U -> "h1000".U,
-            13.U -> "h2000".U,
-            14.U -> "h4000".U,
-            15.U -> "h8000".U
-        )
-    )
+    // io.out := MuxLookup(io.in, 0.U)(
+    //     Seq(
+    //         0.U -> "h1".U,
+    //         1.U -> "h2".U,
+    //         2.U -> "h4".U,
+    //         3.U -> "h8".U,
+    //         4.U -> "h10".U,
+    //         5.U -> "h20".U,
+    //         6.U -> "h40".U,
+    //         7.U -> "h80".U,
+    //         8.U -> "h100".U,
+    //         9.U -> "h200".U,
+    //         10.U -> "h400".U,
+    //         11.U -> "h800".U,
+    //         12.U -> "h1000".U,
+    //         13.U -> "h2000".U,
+    //         14.U -> "h4000".U,
+    //         15.U -> "h8000".U
+    //     )
+    // )
+
+    val lut = VecInit((0 until 16).map{ i => (1 << i).toInt.U(16.W)})
+    io.out := lut(io.in)
 }
 
 // 测试命令：
