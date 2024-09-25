dde17c90547f9c6083d1774707183c7f28219926
stozn <stozn.cn@outlook.com>
2024-09-25 16:30:27 +0800

    exp 8

--- src/main/scala/exp8/todo/DataTrans.scala
+++ src/main/scala/exp8/todo/DataTrans.scala
@@ -12,11 +12,18 @@ class DataSender extends Module {
         val out     = DecoupledIO(UInt(Width.W))
         val debugIO = Vec(num, UInt(Width.W))
     })
-    val addrCnt = RegInit(0.U(log2Up(num + 1).W))
+    val addrCnt = RegInit(0.U(log2Up(num).W))
     val dataReg = RegInit(VecInit(List.fill(num)(Random.between(0, 256).U(8.W))))
+    val readyCnt = RegInit(0.U(10.W))
 
     // todo
-    io.out := DontCare
+    io.out.valid := true.B
+    io.out.bits := 0.U
+    readyCnt := readyCnt + 1.U
+    when((readyCnt % 3.U) === 1.U){
+        io.out.bits := dataReg(addrCnt)
+        addrCnt := addrCnt + 1.U
+    }
 
     // don't move below
     for (i <- 0 until num) {
@@ -40,8 +47,13 @@ class DataReceiver extends Module {
     io.in.ready := ((readyCnt % 3.U) === 1.U)
 
     val dataReg = RegInit(VecInit(List.fill(num)(0.U(8.W))))
-    val addrCnt = RegInit(0.U(log2Up(num + 1).W))
+    val addrCnt = RegInit(0.U(log2Up(num).W))
 
+    when(io.in.fire){
+        dataReg(addrCnt) := io.in.bits
+        addrCnt := addrCnt + 1.U
+    }
+    
     // don't move below
     for (i <- 0 until num) {
         io.debugIO(i) := dataReg(i)
@@ -59,6 +71,7 @@ class DataTrans extends Module {
 
     val sender   = Module(new DataSender)
     val receiver = Module(new DataReceiver)
+    receiver.io.in <> sender.io.out
 
     // don't move below
     io.senderData   := sender.io.debugIO
